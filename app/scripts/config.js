eeaf27adc94eee937c8c4dd70099028221485f3e
Michael Pritchard <michael.pritchard@phila.gov>
2015-02-02 16:08:15 -0500

    Changed gis.phila.gov to api.phila.gov
    Changed GIS web service references from gis.phila.gov to api.phila.gov.

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -13,32 +13,32 @@ define([
     config.regionMappings = {
       cd: {
         fullName: 'Council District',
-        apiRoot: 'http://gis.phila.gov/arcgis/rest/services/PhilaGov/ServiceAreas/MapServer/2',
+        apiRoot: 'http://api.phila.gov/arcgis/rest/services/PhilaGov/ServiceAreas/MapServer/2',
         field: 'DIST_NUM'
       },
       rco: {
         fullName: 'Registered Community Organization',
-        apiRoot: 'http://gis.phila.gov/arcgis/rest/services/PhilaGov/RCO/MapServer/0',
+        apiRoot: 'http://api.phila.gov/arcgis/rest/services/PhilaGov/RCO/MapServer/0',
         field: 'ORGANIZATION_NAME'
       },
       planning: {
         fullName: 'Planning District',
-        apiRoot: 'http://gis.phila.gov/arcgis/rest/services/PhilaGov/ServiceAreas/MapServer/20',
+        apiRoot: 'http://api.phila.gov/arcgis/rest/services/PhilaGov/ServiceAreas/MapServer/20',
         field: 'DIST_NAME'
       }
     };
 
     config.rcoList = {
-      url: 'http://gis.phila.gov/arcgis/rest/services/PhilaGov/RCO/MapServer/0/query?where=1%3D1&outFields=ORGANIZATION_NAME&returnGeometry=true&returnIdsOnly=false&returnCountOnly=false&orderByFields=ORGANIZATION_NAME&returnZ=false&returnM=false&returnDistinctValues=false&f=pjson'
+      url: 'http://api.phila.gov/arcgis/rest/services/PhilaGov/RCO/MapServer/0/query?where=1%3D1&outFields=ORGANIZATION_NAME&returnGeometry=true&returnIdsOnly=false&returnCountOnly=false&orderByFields=ORGANIZATION_NAME&returnZ=false&returnM=false&returnDistinctValues=false&f=pjson'
     };
 
     config.basemap = {
-      url: 'http://gis.phila.gov/arcgis/rest/services/BaseMaps/Hybrid_WM/MapServer/tile/{z}/{y}/{x}'
+      url: 'http://api.phila.gov/arcgis/rest/services/BaseMaps/Hybrid_WM/MapServer/tile/{z}/{y}/{x}'
     };
 
     config.appeal = {
       /*jshint quotmark:double */
-      url: "http://gis.phila.gov/arcgis/rest/services/PhilaGov/Construction/MapServer/5/query?f=json&returnIdsOnly=false&returnCountOnly=false&outFields=VIOLATION_ADDRESS,TYPE,APPEAL_NUM,PERMIT_NO,GROUNDS,PRIMARY_APPLICANT,DATE_SCHEDULED,APPEAL_KEY&&where=APPEAL_NUM='<%= appealNum %>'&outSR=4326&returnGeometry=true"
+      url: "http://api.phila.gov/arcgis/rest/services/PhilaGov/Construction/MapServer/5/query?f=json&returnIdsOnly=false&returnCountOnly=false&outFields=VIOLATION_ADDRESS,TYPE,APPEAL_NUM,PERMIT_NO,GROUNDS,PRIMARY_APPLICANT,DATE_SCHEDULED,APPEAL_KEY&&where=APPEAL_NUM='<%= appealNum %>'&outSR=4326&returnGeometry=true"
     };
 
     /*jshint quotmark:single */
